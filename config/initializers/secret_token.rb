f376974e34f16ea746c8c2ed58219a2871948b01
Rene de la Garza <rene@hexsoft.pro>
2012-05-12 22:19:30 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quinielas::Application.config.secret_token = '9341580bbea056baa4187469a222614e4eb4b64f5ac2f4d76b95ef08224443a258950590989ee4a99355c228fda68da4a9ce17a15697ea33456b04b38cb64d30'
